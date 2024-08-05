f61f1c2e9a3914b8e9d0aaeacea74f9d9c5ea945
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-05 15:59:17 -0300

    added initial files

---
+++ 01_HelloWorld.ts
@@ -0,0 +1,12 @@
+/*
+1) Write a program that displays the message "Hello, World!" on the screen.
+*/
+
+function main(): void {
+  function helloWorld(): void {
+    console.log("Hello World !");
+  }
+  helloWorld();
+}
+
+main();
