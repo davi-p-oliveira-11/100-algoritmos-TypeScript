c8992d6fc07fc1ed3b385a1011f445d6810f6caa
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-06 14:31:48 -0300

    added algorithm 44

---
+++ 44_CustomCounter.ts
@@ -0,0 +1,33 @@
+/*
+ 44) Create an algorithm that reads the initial value of the count, the final value, and the increment, then displays all the values within the interval:
+Example:
+Enter the first value: 3
+Enter the last value: 10
+Enter the increment: 2
+Counting: 3 5 7 9 Done!
+*/
+
+import { prompt, pressEnterToExit } from "./utils/inputUtils";
+
+function main(): void {
+  function algorithm(): void {
+    let firstValue: number = parseInt(prompt("Enter the first value: "));
+    let lastValue: number = parseInt(prompt("Enter the last value: "));
+    let increment: number = parseInt(
+      prompt("Type the value of the increment: ")
+    );
+
+    let counter: number = firstValue;
+
+    while (counter <= lastValue) {
+      console.log(counter);
+      counter += increment;
+    }
+
+    console.log("It's over !");
+  }
+  algorithm();
+}
+
+main();
+pressEnterToExit();
