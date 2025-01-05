f3d70c929613bad0a38bea08b1ce5fdb003d847e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-04 23:49:19 -0300

    updated algorithm 01

--- 01_HelloWorld.ts
+++ 01_HelloWorld.ts
@@ -2,13 +2,11 @@
 1) Write a program that displays the message "Hello, World!" on the screen.
 */
 
-import { pressEnterToExit } from "./utils/inputUtils";
-
 // function that shows the message
 
 function showMessage(message: string): string {
   return `${message}`;
 }
 
-console.log(showMessage("Hello World !"));
-pressEnterToExit();
+const result = showMessage("Hello World !");
+console.log(result);
