c906ce7d9617bb7911ac44f72ce34cbb54e9ed45
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-06 23:31:32 -0300

    updated algo 04

--- 04_SumOfTwo.ts
+++ 04_SumOfTwo.ts
@@ -5,19 +5,6 @@ Enter a value: 8
 Enter another value: 5
 The sum of 8 and 5 is equal to 13.
 */
-import { prompt, pressEnterToExit } from "./utils/inputUtils";
-
-function main(): void {
-  function sumOfTwo(): void {
-    const num1: number = parseInt(prompt("Enter a number: "));
-    const num2: number = parseInt(prompt("Enter another number: "));
-    const sum: number = num1 + num2;
-
-    console.log(`The sum between ${num1} and ${num2} is ${sum}`);
-  }
-
-  sumOfTwo();
-}
 
 function sum(num1: number, num2: number, result?: number): number | string {
   result = num1 + num2;
