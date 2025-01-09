d2268dbbea6e42c7cd5e3c4d7bee5e9932a9f1f6
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-08 22:38:09 -0300

    added final implementation

--- 09_CurrencyConverter.ts
+++ 09_CurrencyConverter.ts
@@ -1,25 +1,7 @@
 /* 
-
 9) Write an algorithm that reads how much money a person has in their wallet (in R$) 
 and shows how many dollars they can buy. Consider US$1.00 = R$3.45.
 */
-
-import { prompt, pressEnterToExit } from "./utils/inputUtils";
-
-function main(): void {
-  function currencyConverter1(): void {
-    const avaiableBRL: number = parseFloat(
-      prompt("How many BRL do you have in your pocket ?")
-    );
-    const dollarAmount: number = avaiableBRL / 3.45;
-
-    console.log(`You can buy a total of ${dollarAmount.toFixed(2)} USD`);
-  }
-  currencyConverter1();
-}
-
-main();
-pressEnterToExit();
 
 function currencyConverter(avaiableBRL: number): string | number {
   const dollarAmount: number = avaiableBRL / 3.45;
