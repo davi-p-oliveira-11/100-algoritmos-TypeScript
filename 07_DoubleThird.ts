5b1a00079b263969ce5f840d7b55477f722837a5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-08 22:25:45 -0300

    added three implementations

--- 07_DoubleThird.ts
+++ 07_DoubleThird.ts
@@ -7,27 +7,14 @@ The double of 3.5 is 7.0
 One-third of 3.5 is 1.16666
 */
 
-import { prompt, pressEnterToExit } from "./utils/inputUtils";
-
-function main(): void {
-  function calculate(): void {
-    const num1: number = parseFloat(prompt("Type a number: "));
-    const double: number = num1 * 2;
-    const third: number = num1 / 3;
-
-    console.log(`The double of ${num1} is ${double.toFixed(2)}`);
-    console.log(`The double of ${num1} is ${third.toFixed(2)}`);
-  }
-  calculate();
-}
-
-main();
-pressEnterToExit();
-
 function calculateNums(num1: number): number | string {
   const double: number = num1 * 2;
   const third: number = num1 / 3;
 
   return `The double of ${num1} is ${double.toFixed(2)}
-The double of ${num1} is ${third.toFixed(2)}`;
+The third part of ${num1} is ${third.toFixed(2)}`;
 }
+
+console.log(calculateNums(3));
+console.log(calculateNums(4));
+console.log(calculateNums(10));
