26bf377fa5e65718afbf85c678d0595dbc7f6f36
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-08 22:37:35 -0300

    commit before deleting the old code

--- 09_CurrencyConverter.ts
+++ 09_CurrencyConverter.ts
@@ -7,7 +7,7 @@ and shows how many dollars they can buy. Consider US$1.00 = R$3.45.
 import { prompt, pressEnterToExit } from "./utils/inputUtils";
 
 function main(): void {
-  function currencyConverter(): void {
+  function currencyConverter1(): void {
     const avaiableBRL: number = parseFloat(
       prompt("How many BRL do you have in your pocket ?")
     );
@@ -15,8 +15,20 @@ function main(): void {
 
     console.log(`You can buy a total of ${dollarAmount.toFixed(2)} USD`);
   }
-  currencyConverter();
+  currencyConverter1();
 }
 
 main();
 pressEnterToExit();
+
+function currencyConverter(avaiableBRL: number): string | number {
+  const dollarAmount: number = avaiableBRL / 3.45;
+
+  return `You can buy a total of ${dollarAmount.toFixed(2)} USD`;
+}
+
+console.log(currencyConverter(2));
+console.log(currencyConverter(14));
+console.log(currencyConverter(7));
+console.log(currencyConverter(80));
+console.log(currencyConverter(100));
