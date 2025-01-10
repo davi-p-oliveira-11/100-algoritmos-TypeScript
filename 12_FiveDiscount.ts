7171f5c92503df52c981b7c64f71cc9cc78f09dd
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-10 00:49:50 -0300

    added updated implementation

--- 12_FiveDiscount.ts
+++ 12_FiveDiscount.ts
@@ -23,3 +23,19 @@ function main(): void {
 
 main();
 pressEnterToExit();
+
+function algorithm12(productPrice: number): string {
+  function calculateDiscount(productPrice: number): number {
+    return productPrice - productPrice * 0.05;
+  }
+
+  const finalPrice = calculateDiscount(productPrice);
+
+  return `The price with discount is : ${finalPrice.toFixed(2)}`;
+}
+
+console.log(algorithm12(100));
+console.log(algorithm12(300));
+console.log(algorithm12(5000));
+console.log(algorithm12(40));
+console.log(algorithm12(150));
