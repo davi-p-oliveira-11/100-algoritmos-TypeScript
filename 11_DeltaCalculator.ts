f993367c58852d4fcabe4ae7c0233d03eaf376ab
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-10 00:19:13 -0300

    added final implementation

--- 11_DeltaCalculator.ts
+++ 11_DeltaCalculator.ts
@@ -3,24 +3,6 @@
  of a quadratic equation and shows the value of Delta.
 */
 
-import { prompt, pressEnterToExit } from "./utils/inputUtils";
-
-function main(): void {
-  function calculateDelta1(): void {
-    const valueOfA: number = parseInt(prompt("Enter the value of A: "));
-    const valueOfB: number = parseInt(prompt("Enter the value of B: "));
-    const valueOfC: number = parseInt(prompt("Enter the value of C: "));
-
-    const delta: number = valueOfB * valueOfB - 4 * (valueOfA * valueOfC);
-
-    console.log(`The value of delta is equal to: ${delta}`);
-  }
-  calculateDelta1();
-}
-
-main();
-pressEnterToExit();
-
 function algorithm11(
   valueOfA: number,
   valueOfB: number,
