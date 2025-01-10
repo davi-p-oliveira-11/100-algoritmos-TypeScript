c7b1f76e6c0936baf9c74fda1fc169a976ff04e1
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-10 00:18:29 -0300

    test before implementation

--- 11_DeltaCalculator.ts
+++ 11_DeltaCalculator.ts
@@ -6,7 +6,7 @@
 import { prompt, pressEnterToExit } from "./utils/inputUtils";
 
 function main(): void {
-  function calculateDelta(): void {
+  function calculateDelta1(): void {
     const valueOfA: number = parseInt(prompt("Enter the value of A: "));
     const valueOfB: number = parseInt(prompt("Enter the value of B: "));
     const valueOfC: number = parseInt(prompt("Enter the value of C: "));
@@ -15,8 +15,30 @@ function main(): void {
 
     console.log(`The value of delta is equal to: ${delta}`);
   }
-  calculateDelta();
+  calculateDelta1();
 }
 
 main();
 pressEnterToExit();
+
+function algorithm11(
+  valueOfA: number,
+  valueOfB: number,
+  valueOfC: number
+): string {
+  function calculateDelta(
+    valueOfA: number,
+    valueOfB: number,
+    valueOfC: number
+  ): number {
+    return valueOfB * valueOfB - 4 * (valueOfA * valueOfC);
+  }
+
+  const delta = calculateDelta(valueOfA, valueOfB, valueOfC);
+
+  return `The value of delta is equal to: ${delta}`;
+}
+
+console.log(algorithm11(10, 5, 12));
+console.log(algorithm11(1, 5, 17));
+console.log(algorithm11(10, 8, 9));
