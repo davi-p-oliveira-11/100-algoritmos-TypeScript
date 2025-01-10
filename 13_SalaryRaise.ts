5a8a929b4964aabf45cc88217b6d6a1e2078d9f2
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-10 01:10:42 -0300

    added final implementation

--- 13_SalaryRaise.ts
+++ 13_SalaryRaise.ts
@@ -3,25 +3,6 @@
  calculates, and shows their new salary with a 15% increase.
 */
 
-import { prompt, pressEnterToExit } from "./utils/inputUtils";
-
-function main(): void {
-  function calculateSalary(): void {
-    const salaryValue: number = parseFloat(
-      prompt("What is the value of your salary ?")
-    );
-
-    const calculateRaise: number = salaryValue * 0.15;
-    const newSalary: number = salaryValue + calculateRaise;
-
-    console.log(`Your new salary with a raise of 15% is: ${newSalary}`);
-  }
-  calculateSalary();
-}
-
-main();
-pressEnterToExit();
-
 function algorithm13(salary: number): string {
   function calculateSalary(salary: number): number {
     return salary + salary * 0.15;
@@ -33,3 +14,6 @@ function algorithm13(salary: number): string {
 }
 
 console.log(algorithm13(100));
+console.log(algorithm13(2500));
+console.log(algorithm13(3800));
+console.log(algorithm13(5000));
