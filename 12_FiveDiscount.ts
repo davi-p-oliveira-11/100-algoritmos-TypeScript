63aec5e34356a90433b44678f8d14ff5cfad567b
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-10 01:00:45 -0300

    added algo 12 final implementation

--- 12_FiveDiscount.ts
+++ 12_FiveDiscount.ts
@@ -3,27 +3,6 @@
  calculates, and shows its PROMOTIONAL PRICE with a 5% discount.
 */
 
-import { prompt, pressEnterToExit } from "./utils/inputUtils";
-
-function main(): void {
-  function calculateDiscount(): void {
-    const productPrice: number = parseFloat(
-      prompt("How much the product costs ? ")
-    );
-
-    const discount: number = productPrice * 0.05;
-    const finalPrice: number = productPrice - discount;
-
-    console.log(`The original price of the product is: ${productPrice}`);
-    console.log(`The price with discount is : ${finalPrice}`);
-  }
-
-  calculateDiscount();
-}
-
-main();
-pressEnterToExit();
-
 function algorithm12(productPrice: number): string {
   function calculateDiscount(productPrice: number): number {
     return productPrice - productPrice * 0.05;
