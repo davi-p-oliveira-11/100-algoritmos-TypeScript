f8416f69c6f5d631c07d2f34666642813db4e797
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-13 12:34:12 -0300

    commit before implement the new one

--- 14_RentCalculator.ts
+++ 14_RentCalculator.ts
@@ -35,3 +35,30 @@ function main(): void {
 
 main();
 pressEnterToExit();
+
+function algorithm14(daysRented: number, distanceTraveled: number): string {
+  function calculateCostPerDay(daysRented: number): number {
+    return daysRented * 90;
+  }
+
+  function costForDistance(distanceTraveled: number): number {
+    return distanceTraveled * 0.2;
+  }
+
+  const costDays: number = calculateCostPerDay(daysRented);
+  const costDistance: number = calculateCostPerDay(distanceTraveled);
+
+  return `The customer drove ${distanceTraveled} km with the car, 
+        rented the car for ${daysRented} days
+        which cost ${costDays.toFixed(2)} dollars for the rental days 
+        and ${costDistance.toFixed(2)} dollars for the kilometers driven, 
+        the total cost the customer must pay is: ${costDays + costDistance} 
+        USD
+      `;
+}
+
+console.log(algorithm14(1, 1));
+console.log(algorithm14(12, 800));
+console.log(algorithm14(5, 150));
+console.log(algorithm14(30, 1200));
+console.log(algorithm14(3, 100));
