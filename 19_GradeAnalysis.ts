fb2efd38bae97d1a1125cd4873efff3d90175286
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-13 14:13:30 -0300

    commit before deleting the old code

--- 19_GradeAnalysis.ts
+++ 19_GradeAnalysis.ts
@@ -5,6 +5,7 @@ In the end, analyze the average and show whether
 the student had good performance (if it was above the average of 7.0).
 */
 
+/*
 import { prompt, pressEnterToExit } from "./utils/inputUtils";
 
 function main(): void {
@@ -27,3 +28,33 @@ function main(): void {
 
 main();
 pressEnterToExit();
+
+*/
+
+function gradeAnalysis(name: string, grade1: number, grade2: number): string {
+  function calculateAverage(grade1: number, grade2: number): number {
+    return (grade1 + grade2) / 2;
+  }
+
+  const average: number = calculateAverage(grade1, grade2);
+
+  function analyzeGrade(average: number): string | number {
+    if (average >= 7) {
+      return "Student Passed";
+    } else {
+      return "Student failed";
+    }
+  }
+
+  const performance: string | number = analyzeGrade(average);
+
+  return `
+  Student ${name} has an average of ${average.toFixed(2)} and had ${performance}
+  `;
+}
+
+console.log(gradeAnalysis("John", 9, 2));
+console.log(gradeAnalysis("Bea", 2, 5));
+console.log(gradeAnalysis("Carl", 10, 7));
+console.log(gradeAnalysis("Leo", 2, 6));
+console.log(gradeAnalysis("Alice", 5, 8));
