f1d9a20c3780c2199a9f65507b7bacfbe2b5b7af
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-13 13:39:22 -0300

    commit before deletin the old solution

--- 17_SpeedMonitor.ts
+++ 17_SpeedMonitor.ts
@@ -30,3 +30,22 @@ function main(): void {
 
 main();
 pressEnterToExit();
+
+function algorithm17(carSpeed: number): string | number | undefined {
+  if (carSpeed > 80) {
+    return `The car reached a speed of ${carSpeed}, exceeding ${
+      carSpeed - 80
+    } km/h
+     of the allowed speed, The driver wiil have to pay a fine of ${
+       (carSpeed - 80) * 5
+     }`;
+  } else {
+    return "The driver did not exceed the speed limit, so they will not have to pay a fine.";
+  }
+}
+
+console.log(algorithm17(100));
+console.log(algorithm17(20));
+console.log(algorithm17(13));
+console.log(algorithm17(150));
+console.log(algorithm17(200));
