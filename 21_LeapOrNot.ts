151e14e33ea83acc4e40fddbb61606cf3a25cbef
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-14 20:20:58 -0300

    updated solution

--- 21_LeapOrNot.ts
+++ 21_LeapOrNot.ts
@@ -2,24 +2,6 @@
 21) Write an algorithm that reads a specific year and shows whether it is a LEAP YEAR or not."
 */
 
-import { prompt, pressEnterToExit } from "./utils/inputUtils";
-
-function main(): void {
-  function leapOrNot(): void {
-    const year: number = parseInt(prompt("Type a year: "));
-
-    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-      console.log(`${year} is leap`);
-    } else {
-      console.log(`${year} is not leap`);
-    }
-  }
-  leapOrNot();
-}
-
-main();
-pressEnterToExit();
-
 function algorithm21(year: number): string | number {
   if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
     return `${year} is leap`;
